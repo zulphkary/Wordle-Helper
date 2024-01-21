12518b92970a57a0771f271dd270ed8216d309c2
zulphkary <148035091+zulphkary@users.noreply.github.com>
2024-01-20 20:19:53 -0800

    new feature

---
+++ worldlePredictor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+
+std::vector<std::string> theWords;
+int counts[130]; // 5 words * 26 char in alphabet
+
+using namespace std;
+
+
+int main ()
+{
+    
+
+}
